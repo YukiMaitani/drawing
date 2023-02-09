d082274806676f682ba03ad50ffb76f4bb230052
YukiMaitani <fightzoi1997@gmail.com>
2023-02-09 21:45:12 +0900

    プロジェクトの作成

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:drawing/pages/draw/draw_page.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DrawPage(),
+    );
+  }
+}
